6e5f6a9fe727dafaf2f46b80d46b93247836d1fa
MrWook <mr.wook@web.de>
2017-10-03 10:50:16 +0200

    - add function to request php error - show php error

--- public/views/projects/php_error_handler/php_error_handler.js
+++ public/views/projects/php_error_handler/php_error_handler.js
@@ -3,11 +3,23 @@ const phpErrorHandler = angular.module('phpErrorHandler', []);
 
 phpErrorHandler.controller('phpErrorHandlerCtrl',['$scope', '$http', function($scope, $http) {
 	$scope.error = {};
+	$scope.timeout_in_progress = false;
 	$scope.click = function(type){
-		$http.post('/php_error', type)
+		if(type == 'timeout')
+			$scope.timeout_in_progress = true;
+		$http.post('/php_error', {type: type})
 			.then(function(response) {
-				console.log(response);
-				$scope.error = response.data;
+				//show appropriate info
+				$scope.message = {};
+				$scope.message.type = response.data.type;
+				$scope.message.text = response.data.text;
+				$scope.message.data = response.data.data;
+				$scope.timeout_in_progress = false;
 			});
-	}
+	};
+
+	//close alert window
+	$scope.closeAlert = function() {
+		$scope.message = undefined;
+	};
 }]);
