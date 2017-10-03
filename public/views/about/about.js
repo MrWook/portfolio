031e80a125430d8bf08338ac39cb863b20779f06
MrWook <mr.wook@web.de>
2017-10-03 10:48:29 +0200

    - add information under the picture

--- public/views/about/about.js
+++ public/views/about/about.js
@@ -1,4 +1,12 @@
 'use strict';
-angular.module('portfolio').controller('AboutCtrl',['$scope', '$rootScope', function($scope, $rootScope){
+angular.module('portfolio').controller('AboutCtrl',['$scope', 'PPconfig', function($scope, PPconfig){
 	$scope.image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII=';
+
+	function calculateAge(birthday){
+		let ageDifMs = Date.now() - birthday.getTime();
+		let ageDate = new Date(ageDifMs);
+		return Math.abs(ageDate.getUTCFullYear() - 1970);
+	}
+
+	$scope.age = calculateAge(new Date(PPconfig.birthday));
 }]);
