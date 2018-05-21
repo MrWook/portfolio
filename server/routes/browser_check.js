299fc320e62069f47c92f6947e354320e2f11637
MrWook <mr.wook@web.de>
2018-05-21 12:05:50 +0200

    - change path

--- server/routes/browser_check.js
+++ server/routes/browser_check.js
@@ -11,17 +11,17 @@ router.get('/*', function(req, res, next) {
 		browserVersion = browser.version.split(".",1);
 	if(browserName == 'IE'){
 		//load html file if the browser is an IE
-		res.sendFile('./public/main/ie.html', {root: req.rootPath});
+		res.sendFile('./public/views/ie.html', {root: req.rootPath});
 	}else if((browserName == 'Firefox' && browserVersion <= 53) ||
 		(browserName == 'Chrome' && browserVersion <= 50) ||
 		(browserName == 'Canary' && browserVersion <= 32) ||
 		(browserName == 'Safari' && browserVersion <= 10) ||
 		(browserName == 'Opera' && browserVersion <= 46)){
 		//load html file if the browser is outdated
-		res.sendFile('./public/main/update.html', {root: req.rootPath});
+		res.sendFile('./public/views/update.html', {root: req.rootPath});
 	}else{
 		//load the single view file (angular will handle the page changes on the front-end)
-		res.sendFile('./public/main/index.html', {root: req.rootPath});
+		res.sendFile('./public/views/index.html', {root: req.rootPath});
 	}
 });
 module.exports = router;
